44748addd3e925a94bccf0a1d8efe32a16a4b9e9
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-09-06 20:41:29 +0000

    Lesson 6

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-alert('Hello Javascript')
+// single line comment
+// the first alert
+alert('Hello Javascript'); //this is a statement. Each statement ends with semicolon
+
+// the second alert
+/*
+alert('Hello Javascript');
+
+- Javascript is case sensitive
+- Contains many statements, all ending with a semicolon ;
+- Not sensitive to whitespace or the breaks
+- Javascripts runs from top to bottom
+- Write one-line comments using //
+- Write multi line comments using /**/
+*/ 
