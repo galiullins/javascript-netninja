3bd0c146c5b4cd372e12b81bc869033e68ed0d9a
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-09-10 21:14:16 +0000

    #20 - Functions

--- test.js
+++ test.js
@@ -12,9 +12,23 @@ if (myAge > 30) {
 }
 */
 
-var links = document.getElementsByTagName('a');
+/* var links = document.getElementsByTagName('a');
 
 for ( x=0; x < links.length; x++){
 
     links[x].className = "link-" + x;
 }
+*/
+
+function getAverage(a,b){
+
+    var average = (a+b) / 2;
+    console.log (average);
+    return average;
+}
+
+
+var myResult= getAverage(7,8);
+
+console.log("the average is:" + myResult);
+
