63244d62f7813e4e3c69ee89c84e91161b80f360
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-09-14 21:30:32 +0000

    Add and remove elements from DOM

--- test.js
+++ test.js
@@ -32,3 +32,23 @@ var myResult= getAverage(7,8);
 
 console.log("the average is:" + myResult);
 
+//lesson 37 
+/*
+var newLi = document.createElement("li");
+var newA = document.createElement("a");
+var menu =document.getElementById("main-nav").getElementsByTagName("ul")[0];
+menu.appendChild(newLi);
+newLi.appendChild(newA);
+newA.innerHTML="Link6";
+newA.setAttribute("href", "#");
+menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);
+*/
+
+// Lesaon 38 Removing Elements from the DOM
+/*
+var parent = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[0];
+parent.removeChild(child);
+or
+var removed = parent.removeChild(child);
+*/
