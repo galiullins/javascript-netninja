bd92e19bb462ee6c302256256c53da3384260998
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-09-16 21:31:51 +0000

    #40 - The onClick Event

--- test.js
+++ test.js
@@ -52,3 +52,21 @@ parent.removeChild(child);
 or
 var removed = parent.removeChild(child);
 */
+
+//Lesson #40
+
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
+
+button.onclick = function(){
+    if (content.className == "open"){
+            //shrink the box
+            content.className = "";
+            button.innerHTML = "show more"
+    } else {
+            //expand the box
+            content.className ="open";
+            button.innerHTML = "show less"
+    }
+};
+
