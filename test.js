e23a1005b00b31d7f3ae421afd51240bb5e7a834
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-09-22 21:31:35 +0000

    Set and stop timer intervals

--- test.js
+++ test.js
@@ -83,7 +83,28 @@ function setUpEvents(){
         }
 
     setTimeout(showMessage, 2000);
+
+    var colourChanger = document.getElementById("colour-changer");
+    var colours =["red", "blue", "green", "pink"];
+    var counter =0;
+
+    function changeColour(){
+        if (counter >=colours.length){
+            counter = 0;
+        }
+        colourChanger.style.background = colours[counter];
+        counter++;
+    };
+
+    var myTimer = setInterval(changeColour, 3000);
+
+    colourChanger.onclick = function(){
+        clearInterval(myTimer);
+        colourChanger.innerHTML = "Timer stopped";
+    };
+
 }
+
 
 window.onload = function(){
     setUpEvents();    
